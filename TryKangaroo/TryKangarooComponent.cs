423436f034c5c1f851c93a6d0d332b24b965a569
wqs111000 <wqs111000@sina.com>
2017-12-26 16:18:19 +0800

    初步实现了弹簧网法

--- TryKangaroo/TryKangarooComponent.cs
+++ TryKangaroo/TryKangarooComponent.cs
@@ -54,13 +54,22 @@ namespace TryKangaroo
             //0
             pManager.AddGenericParameter("Mesh", "M", "Mesh", GH_ParamAccess.item);
             //1
+            pManager.AddGenericParameter("TargetMesh", "TM", "吸附的曲面", GH_ParamAccess.item);
             //pManager.AddGenericParameter("Point", "P", "Points", GH_ParamAccess.list);
             //2
+            pManager.AddCurveParameter("BoundaryCurves", "BC", "BoundaryCurves", GH_ParamAccess.list);
             //pManager.AddIntegerParameter("TargetLength", "L", "A function determining local edge length", GH_ParamAccess.item);
             //3
+            pManager.AddNumberParameter("PullStrengthM", "PM", "Strength of pull to target Mesh", GH_ParamAccess.item, 1);
             //pManager.AddLineParameter("Line", "L", "Lines", GH_ParamAccess.list);
             //4
+            pManager.AddNumberParameter("PullStrengthC", "PC", "Strength of pull to Boundary Curves", GH_ParamAccess.item, 1);
+            //5
+            pManager.AddNumberParameter("SpringStrength", "S", "Strength of Springs of edges", GH_ParamAccess.item, 1);
+            //6
             pManager.AddBooleanParameter("Reset", "R", "Reset", GH_ParamAccess.item);
+            //7
+            pManager.AddGenericParameter("FixedPoint", "P", "FixedPoint", GH_ParamAccess.list);
         }
 
         /// <summary>
@@ -71,17 +80,19 @@ namespace TryKangaroo
             //pManager.AddTextParameter("Reverse", "R", "Reversed string", GH_ParamAccess.item);
             pManager.AddGenericParameter("Mesh", "A", "Mesh", GH_ParamAccess.item);
             pManager.AddPointParameter("Point", "P", "Point", GH_ParamAccess.list);
-     
+
         }
 
         //private Mesh M = new Mesh();
-       // List<Point3d> FV = new List<Point3d>();
-       // List<Line> FL = new List<Line>();
+        // List<Point3d> FV = new List<Point3d>();
+        // List<Line> FL = new List<Line>();
         //int onMeshStrength = 100;
-       // int restLength = 0;
+        // int restLength = 0;
         KangarooSolver.PhysicalSystem PS = new KangarooSolver.PhysicalSystem();
         List<IGoal> Goals = new List<IGoal>();
+        List<IGoal> GoalList = new List<IGoal>();
         Mesh M = new Mesh();
+        Mesh TargetMesh = new Mesh();
         bool initialized;
         int counter = 0;
         double threshold = 1e-6;
@@ -93,12 +104,21 @@ namespace TryKangaroo
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             bool reset = false;
-            //List<Point3d> FV = new List<Point3d>();
+            List<Point3d> FixedPoint = new List<Point3d>();
+            List<Curve> BoundaryCurves = new List<Curve>();
+            double SpringStrength = 1, PullStrengthM = 1, PullStrengthC = 1;
             DA.GetData<Mesh>(0, ref M); //获取第0个输入值
-            DA.GetData<bool>(1, ref reset);
+            DA.GetData<Mesh>(1, ref TargetMesh); //获取第0个输入值
             //DA.GetDataList<Point3d>(1, FV);
+            DA.GetDataList<Curve>(2, BoundaryCurves);
             //DA.GetDataList<Line>(1, FL);
             // DA.GetData<int>(2, ref restLength);
+            DA.GetData<double>(3, ref PullStrengthM);
+            DA.GetData<double>(4, ref PullStrengthC);
+            DA.GetData<double>(5, ref SpringStrength);
+            DA.GetData<bool>(6, ref reset);
+            DA.GetDataList<Point3d>(7, FixedPoint);
+
             //内部计算过程start   
 
             //initialize the solver
@@ -111,28 +131,52 @@ namespace TryKangaroo
                 Goals = new List<IGoal>();
                 initialized = true;
                 //get the Mesh points and boundary status
-                Point3d[] Pts = M.Vertices.ToPoint3dArray();
+                //数组转列表
+                List<Point3d> Pts = new List<Point3d>(M.Vertices.ToPoint3dArray());
+                List<Point3d> NakedPts = new List<Point3d>();
                 bool[] Naked = M.GetNakedEdgePointStatus();
 
                 for (int i = 0; i < M.Vertices.Count; i++)
                 {
-                    PS.AddParticle(Pts[i], 1); //add a particle for every mesh vertex
+                    //PS.AddParticle(Pts[i], 1); //add a particle for every mesh vertex
                     if (Naked[i])
-                    { Goals.Add(new KangarooSolver.Goals.Anchor(i, Pts[i], 10000)); }// fix the boundaries strongly in place
+                    //{ Goals.Add(new KangarooSolver.Goals.Anchor(i, Pts[i], 10000)); }// fix the boundaries strongly in place
+                    {
+                        NakedPts.Add(Pts[i]);
+                    }
                 }
-
+                Goals.Add(new KangarooSolver.Goals.OnCurve(NakedPts, BoundaryCurves[0], PullStrengthC));
+                Goals.Add(new KangarooSolver.Goals.OnMesh(Pts, TargetMesh, 1));
+                double sum_length = 0;
+                for (int i = 0; i < M.TopologyEdges.Count; i++)
+                {
+                    Line line = M.TopologyEdges.EdgeLine(i);
+                    double length = line.Length;
+                    sum_length += length;
+                }
+                double average_length = sum_length / M.TopologyEdges.Count;
                 for (int i = 0; i < M.TopologyEdges.Count; i++)
                 {
                     var Ends = M.TopologyEdges.GetTopologyVertices(i);
+
                     int Start = M.TopologyVertices.MeshVertexIndices(Ends.I)[0];
                     int End = M.TopologyVertices.MeshVertexIndices(Ends.J)[0];
-                    Goals.Add(new KangarooSolver.Goals.Spring(Start, End, 0, 1)); //for each edge, a spring with rest length 0, and strength 1
+                    //for each edge, a spring with rest length average_length, and strength 1
+                    Goals.Add(new KangarooSolver.Goals.Spring(Pts[Start], Pts[End], average_length, 1));
                 }
-               // foreach (IGoal G in Goals)
-              //  {
-               //     PS.AssignPIndex(G, 0.01);
-               // }
-
+                for (int i = 0; i < FixedPoint.Count; i++)
+                {
+                    Goals.Add(new KangarooSolver.Goals.Anchor( FixedPoint[i], 10000));
+                }// fix the boundaries strongly in place
+                 // foreach (IGoal G in Goals)
+                 //  {
+                 //     PS.AssignPIndex(G, 0.01);
+                 // }
+                foreach (IGoal G in Goals) //Assign indexes to the particles in each Goal:
+                {
+                    PS.AssignPIndex(G, 0.0001); // the second argument is the tolerance distance below which points combine into a single particle
+                    GoalList.Add(G);
+                }
                 #endregion
             }
             /*
@@ -169,45 +213,45 @@ namespace TryKangaroo
                 }
                 */
 
-
+              
             //Step forward, using these goals, with multi-threading on, and stopping if the threshold is reached
-            if (counter==0 || (PS.GetvSum() > threshold && counter < 100))
-            {
-                PS.Step(Goals, true, threshold);
-                counter++;
-            }
-            //GetvSum returns the current kinetic energy
-            //always include a counter to prevent it getting stuck forever in case it cannot reach the given threshold
-
-            //replace the mesh vertices with the relaxed ones
-            M.Vertices.Clear();
-            M.Vertices.AddVertices(PS.GetPositions());
-            //Goals.Add(new KangarooSolver.Goals.Anchor(i, Pts[i], 10000)); 
-            /*
-            for (int i = 0; i < FV.Count; i++)
-            {
-                for (int j = i+1; j < FV.Count; j++)
+            if (counter == 0 || (PS.GetvSum() > threshold && counter < 100))
                 {
-                   // Goals.Add(new KangarooSolver.Goals.Spring(i, j, restLength, 100)); //for each edge, a spring with rest length 0, and strength 1
-                    Goals.Add(new KangarooSolver.Goals.ClampLength(i, j, 10000, restLength, 100));
-                }     
+                    PS.Step(GoalList, true, threshold);
+                    counter++;
+                }
+                //GetvSum returns the current kinetic energy
+                //always include a counter to prevent it getting stuck forever in case it cannot reach the given threshold
+
+                //replace the mesh vertices with the relaxed ones
+                M.Vertices.Clear();
+                M.Vertices.AddVertices(PS.GetPositions());
+                //Goals.Add(new KangarooSolver.Goals.Anchor(i, Pts[i], 10000)); 
+                /*
+                for (int i = 0; i < FV.Count; i++)
+                {
+                    for (int j = i+1; j < FV.Count; j++)
+                    {
+                       // Goals.Add(new KangarooSolver.Goals.Spring(i, j, restLength, 100)); //for each edge, a spring with rest length 0, and strength 1
+                        Goals.Add(new KangarooSolver.Goals.ClampLength(i, j, 10000, restLength, 100));
+                    }     
+                }
+                */
+                //Goals.Add(new KangarooSolver.Goals.OnMesh(FV, M, onMeshStrength));
+
+
+                //GetvSum returns the current kinetic energy
+                //always include a counter to prevent it getting stuck forever in case it cannot reach the given threshold
+
+                //replace the mesh vertices with the relaxed ones
+                //内部计算过程end   
+
+                //Output the mesh, and how many iterations it took to converge
+                DA.SetData(0, M);
+                List<Point3d> pout = new List<Point3d>();
+                pout = PS.GetPositions().ToList();
+                DA.SetDataList(1, pout); //输出第一个输出值
             }
-            */
-            //Goals.Add(new KangarooSolver.Goals.OnMesh(FV, M, onMeshStrength));
-
-
-            //GetvSum returns the current kinetic energy
-            //always include a counter to prevent it getting stuck forever in case it cannot reach the given threshold
-
-            //replace the mesh vertices with the relaxed ones
-            //内部计算过程end   
-
-            //Output the mesh, and how many iterations it took to converge
-            DA.SetData(0, M);
-            List<Point3d> pout = new List<Point3d>();
-            pout = PS.GetPositions().ToList();
-            DA.SetDataList(1, pout); //输出第一个输出值
-        }
 
         /// <summary>
         /// Provides an Icon for every component that will be visible in the User Interface.
